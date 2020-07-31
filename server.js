87394ad646a69485ed38484ed7e62b461ca43b21
jyk524 <67247717+jyk524@users.noreply.github.com>
2020-07-31 15:34:31 -0400

    added dependencies to server.js and listen PORT

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
+// Dependencies
 
+var express = require("express");
+var path = require("path");
+const { allowedNodeEnvironmentFlags } = require("process");
+
+// Setting up Express App
+var app = express();
+var port = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
